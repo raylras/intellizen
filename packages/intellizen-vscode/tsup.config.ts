4ebff873d904671099753bba8a79a561f34d07ad
raylras <raylras@outlook.com>
2025-10-03 20:47:25 +0800

    chore(build): improve dev & prod mode

--- packages/intellizen-vscode/tsup.config.ts
+++ packages/intellizen-vscode/tsup.config.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   entry: ['src/node/extension.ts', 'src/node/langserver.ts'],
   outDir: 'dist/node',
   format: 'cjs',
-  sourcemap: true,
-  minify: true,
   external: ['vscode'],
   noExternal: [/^(?!vscode$)/],
 })
--- packages/intellizen-zenscript/tsup.config.ts
+++ packages/intellizen-zenscript/tsup.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'dist',
-  splitting: false,
-  sourcemap: true,
   clean: true,
-  dts: true,
-  minify: true,
   format: ['esm'],
 })
