639decb75a8e553327355454828c7773d5eaf8a4
raylras <raylras@outlook.com>
2025-10-05 00:27:43 +0800

    chore(build): improve build config and cleanup

--- packages/intellizen-zenscript/tsup.config.ts
+++ packages/intellizen-zenscript/tsup.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['src/index.ts'],
   outDir: 'dist',
-  clean: true,
-  format: ['esm'],
+  format: 'esm',
 })
--- tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { env } from 'node:process'
-import { defineConfig } from 'tsup'
-
-export const DEV_MODE = env.NODE_ENV === 'development'
-
-export default defineConfig({
-  clean: true,
-})
